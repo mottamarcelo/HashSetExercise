ef301bdc242794bf300f126309712e67e6776bf3
Marcelo Motta <128328762+mottamarcelo@users.noreply.github.com>
2025-01-31 19:27:04 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set = new HashSet<int>();
+
+            Console.Write("How many students for course A? ");
+            int studA = int.Parse (Console.ReadLine());
+            for (int i = 0; i < studA; i++)
+            {
+                set.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.Write("How many students for course B? ");
+            int studB = int.Parse(Console.ReadLine());
+            for (int i = 0; i < studB; i++)
+            {
+                set.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.Write("How many students for course C? ");
+            int studC = int.Parse(Console.ReadLine());
+            for (int i = 0; i < studC; i++)
+            {
+                set.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine("Total students: " + set.Count());
+        }
+    }
+}
